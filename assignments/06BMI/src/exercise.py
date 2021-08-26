2b65a082c8026dc9c4bae663cc8147c1f9c261c5
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-25 22:51:05 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+import math
+weight=float(input("Give me your weight in kg:"))
+height=float(input("Give me your height in meters"))
+BMI=((weight)/(height**2))
+print("Your Body Max Index is", BMI)
+if BMI<20:
+    print("underweight")
+if 20<=BMI<25:
+    print("normal")
+if 25<=BMI<30:
+    print("overweight")
+if 30<=BMI<40:
+    print("obesity")
+if BMI>=40:
+    print("morbid obesity")   
+pass
 if __name__=='__main__':
     main()
