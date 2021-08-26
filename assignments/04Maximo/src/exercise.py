2847d30ae5306f621606c95e93fcf747f42573ea
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-26 00:10:28 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,31 +1,25 @@
 def main():
     #escribe tu código abajo de esta línea
     x = int(input("Ingresa el primer número: "))
-    y = int(input("Ingresa el segundo número: "))
-    z = int(input("Ingresa el tercer número: "))
-    if x <= y and x <= z and y <= z:
+y = int(input("Ingresa el segundo número: "))
+z = int(input("Ingresa el tercer número: "))
+if x <= y and x <= z and y <= z:
+        
+        print(z)
+elif x <= y and x <= z and z <= y:
+        
+        print(y)
+elif y <= x and y <= z and x <= z:
+       
+        print(z)
+elif y <= x and y <= z and z <= x:
+        
         print(x)
+elif z <= x and z <= y and x <= y:
+        
         print(y)
-        print(z)
-    elif x <= y and x <= z and z <= y:
-        print(x)
-        print(z)
-        print(y)
-    elif y <= x and y <= z and x <= z:
-        print(y)
-        print(x)
-        print(z)
-    elif y <= x and y <= z and z <= x:
-        print(y)
-        print(z)
-        print(x)
-    elif z <= x and z <= y and x <= y:
-        print(z)
-        print(x)
-        print(y)
-    elif z <= x and z <= y and y <= x:
-        print(z)
-        print(y)
+elif z <= x and z <= y and y <= x:
+        
         print(x)
     pass
 
