25a61f55f9496f5b1c40c37d2073b10c8adf867a
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-25 21:44:45 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,14 +1,13 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    num=int(input("Give me a number"))
-if num>0:
-    print("Your number is positive")
+num=int(input("Give me a number"))
 if num<0:
-    print("Your number is negative")
+    print("Negative")
+if num>0:
+    print("Positive")
 if -1<num<1:
-    pass
-    
+    print("It is zero")
 
 if __name__ == '__main__':
     main()
